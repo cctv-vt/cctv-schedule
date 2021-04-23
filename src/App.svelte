ffc39d4ee9d23d36ab0c0a665d52e019e4e86ad0
Luna Zeitlyn <luna@gobolin.com>
2021-04-22 21:15:35 -0400

    🎉 all: first commit!

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Schedule from './components/Schedule.svelte'
+	import axios from 'axios'
+	
+	let channel
+	axios.get("https://tmtv-schedule.herokuapp.com/").then((response) => {
+		console.log(response.data.rss.channel[0].item[0 ])
+		channel = response.data.rss.channel
+	})
+</script>
+
+<main>
+	{#if channel}
+		<Schedule schedule={channel[0].item}/>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: left;
+		padding: 1em;
+		/* max-width: 240px; */
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	/* @media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
---
+++ src/components/Schedule.svelte
@@ -0,0 +1,17 @@
+<script>
+import { each } from "svelte/internal";
+
+
+   export let schedule
+</script>
+
+<main>
+    <div>
+        <!-- <h3>{title}</h3> -->
+        {#each schedule as scheduleItem, index}
+        <div>
+            <p><strong>{scheduleItem.comments}</strong> {#if scheduleItem.psg_attribute && scheduleItem.psg_attribute[1]} <a href={scheduleItem.psg_attribute[1]._}>{scheduleItem.title}</a>{:else}{scheduleItem.title}{/if}</p>
+        </div>
+        {/each}
+    </div>
+</main>
